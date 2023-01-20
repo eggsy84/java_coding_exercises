afe92d4e50a2d1efd91fa298ff113b9fab511bc1
mousumi-ish <baral_mousumi@yahoo.com>
2023-01-20 11:24:16 +0000

    changes to Exercise001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        //Add your code here
 
         return Character.toUpperCase(word.charAt(0))+word.substring(1);
     }
@@ -19,21 +18,33 @@ public class Exercise001 {
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        double vat = originalPrice + ((vatRate / 100) * originalPrice);
+        return (Math.round(vat * 100.0) / 100.0);
+
     }
 
     public String reverse(String sentence) {
         // Add your code here
         String reversedSentence="";
         for(int i=sentence.length()-1;i>=0;i--){
-            reversedSentence=reversedSentence+sentence.charAt(i) ;
+            reversedSentence = reversedSentence + sentence.charAt(i) ;
         }
         return reversedSentence;
     }
 
-    public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+    public int countLinuxUsers(List<User> users)
+    {
+
+
+        int count = 0;
+       //* advanced for loop syntax for (DateType dt: arrayname)
+        for (User u : users) {
+          if (u.getType().equals("Linux"))
+           // if (u.getUsername().equals("Paul"))
+            count++;
+        }
+        return count;
     }
+
 }
 
